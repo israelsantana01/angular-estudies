882f9d56ae560231cf6ffbc7727f3d25ea549836
israelsantana01 <israel.silva.santana09@gmail.com>
2020-01-27 13:02:08 -0300

    Finish the basic course of Angular.

--- angular-course/src/app/modules/lazy-loading/lazy-loading-routing.module.ts
+++ angular-course/src/app/modules/lazy-loading/lazy-loading-routing.module.ts
@@ -1,10 +1,29 @@
+import { HomelazyComponent } from './../../components/homelazy/homelazy.component';
+import { HeaderComponent } from './../../components/header/header.component';
+import { FooterComponent } from './../../components/footer/footer.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomelazyComponent,
+    children: [
+      {
+        path: 'header',
+        component: HeaderComponent
+      },
+      {
+        path: 'footer',
+        component: FooterComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
+  declarations: [FooterComponent, HeaderComponent, HomelazyComponent],
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
